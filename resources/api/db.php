cc893dde1832614dad3a8f1bf0bf42c13328a244
Luuk Muskens <luuk@travpromobile.com>
2025-11-05 10:39:15 +0100

    DB dubbele code debuggen

--- resources/api/db.php
+++ resources/api/db.php
@@ -1,10 +1,4 @@
 <?php
-$host = 'localhost';       // Usually localhost
-$db   = 'your_database';   // Replace with your database name
-$user = 'your_username';   // Replace with your MySQL username
-$pass = 'your_password';   // Replace with your MySQL password
-$charset = 'utf8mb4';
-
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 $options = [
